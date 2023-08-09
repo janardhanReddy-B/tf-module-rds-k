e6289e7fa9fa2cc7a3766ffc94f54d686f515d8a
JanardhanReddy <bommireddy246@gmail.com>
2023-08-09 14:42:06 +0100

    terraform

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "env" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "dbname" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+
+
+variable "skip_final_snapshot" {}
+variable "vpc" {}
+variable "kms_key_id" {}
+variable "storage_encrypted" {}
+
+variable "instance_count" {}
+variable "instance_class" {}
